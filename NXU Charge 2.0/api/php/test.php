627633ab29473e825e74ef9cf13b3bdc3af5831d
thisish <thisish@foxmail.com>
2024-02-28 21:28:40 +0800

    新增一些说明，添加测试

---
+++ NXU Charge 2.0/api/php/test.php
@@ -0,0 +1,65 @@
+<?php
+// 这里执行一些服务器检测及测试函数
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require './vendor/autoload.php';
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Asia/Shanghai');
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+try {
+    //Server settings
+    //Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    //Send using SMTP
+    $mail->isSMTP();
+    //Set the SMTP server to send through，这里我用的QQ邮箱的，其他邮箱同理
+    $mail->Host       = 'smtp.qq.com';
+    //Enable SMTP authentication
+    $mail->SMTPAuth   = true;
+    //SMTP username
+    $mail->Username   = 'thisish@foxmail.com';
+    //SMTP password 
+    $mail->Password   = 'tfmvhtvkzvhwdgbd';
+    //Enable implicit TLS encryption
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    //这里的端口可以去QQ邮箱查一下
+    $mail->Port       = 465;
+ 
+    //Recipients
+    $mail->setFrom('thisish@foxmail.com', '11');
+    //Add a recipient
+    $mail->addAddress('3259460561@qq.com');
+    //Name is optional
+    // $mail->addAddress('多个收件人邮箱');
+    // $mail->addReplyTo('用于接受回复的邮箱', 'Information');
+    // $mail->addCC('抄送邮箱');
+    // $mail->addBCC('收件人邮箱（能收到邮件但是不会显示邮箱）');
+ 
+    //Attachments
+    //Add attachments
+    // $mail->addAttachment('这里填要发送的附件的【路径/附件】');
+    //Optional name
+    // $mail->addAttachment('这里填要发送的附件的【路径/附件】', '新名称');
+ 
+    //Content
+    //Set email format to HTML
+    $mail->isHTML(true);
+    //这里是邮件的主题
+    $mail->Subject = 'This is a emailTest of php';
+    //这里是邮件的正文内容
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    //这是非html邮件客户端的纯文本正文
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+ 
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# NXU Charge
+<h1 align="center">NXU Charge</h1>
 
-`Version 1.1.4`
+<div align="center">
+    <img src="https://img.shields.io/badge/author-H-4472c4">
+    <img src="https://img.shields.io/github/license/this-is-h/nxu-charge" />
+    <img src="https://img.shields.io/github/languages/code-size/this-is-h/nxu-charge" />
+</div>
+
+> ### 前言
+>
+> 你是否在下课后赶到充电桩确没有位置
 
 这是一个服务于校园内电动车充电问题的项目
 
 ## 项目说明
 
 先这样吧，懒得写了 :sleeping: :sleeping:
+
+## Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=this-is-h/nxu-charge&type=Date)](https://star-history.com/#this-is-h/nxu-charge&Date)
