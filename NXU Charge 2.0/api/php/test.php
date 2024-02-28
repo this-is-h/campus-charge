4967e10303aa977b722e06d7e9031c0ceaddbc0a
thisish <thisish@foxmail.com>
2024-02-28 22:22:04 +0800

    部署测试

--- NXU Charge 2.0/api/php/test.php
+++ NXU Charge 2.0/api/php/test.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
