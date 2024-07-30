ca44f34a1653dc1f64a8df66d8ffc0a22794d21b
this-is-h <thisish@foxmail.com>
2024-07-30 12:20:16 +0800

    v1.3.2

--- NXU Charge 2.0/src/public.js
+++ NXU Charge 2.0/src/public.js
@@ -1,3 +1,3 @@
-export const VERSION = "1.3.0";
+export const VERSION = "1.3.2";
 export const DATA_URL = ""; // 设置数据源
-export const UPDATE_ACTIVE_NAME = '18';
+export const UPDATE_ACTIVE_NAME = '19';
